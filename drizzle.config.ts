481033c249c98bdd195676bec4d0926e96e53e9f
Jacob Marshall <me@jhqcat.com>
2023-11-07 21:08:25 +0000

    🔒️ Remove secret leak (Creds and URLs rotated)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,7 @@ dotenv.config();
 export default {
     driver: "pg",
     dbCredentials: {
-        //connectionString: process.env.POSTGRES_URL_NON_POOLING as string,
-        connectionString: "postgres://default:z8lxoktUDO3u@ep-icy-haze-43562761-pooler.eu-central-1.postgres.vercel-storage.com/verceldb?ssl=true"
+        connectionString: `${process.env.POSTGRES_URL_NON_POOLING as string}?ssl=true`,
     },
     schema: "./db/schema.ts",
     out: "./db/migrations",
