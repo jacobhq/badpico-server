ca0a814b2d1c61fb4d037f32479979a1ae49a655
Jacob Marshall <me@jhqcat.com>
2023-11-07 20:35:47 +0000

    🗃️ Get the latest attack, not the oldest

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import { handle } from 'hono/vercel'
 import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import * as schema from '../db/schema';
+import { desc } from 'drizzle-orm';
 
 export const config = {
   runtime: 'edge',
@@ -13,7 +14,9 @@ const app = new Hono().basePath('/api')
 const db = drizzle(sql, { schema })
 
 app.get('/', async (c) => {
-  const result = await db.query.attack_schedule.findFirst()
+  const result = await db.query.attack_schedule.findFirst({
+    orderBy: desc(schema.attack_schedule.id)
+  })
   return c.json(result)
 })
 
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,13 @@
 import type { Config } from "drizzle-kit";
- 
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export default {
-  schema: "./db/schema.ts",
-  out: "./db/migrations",
+    driver: "pg",
+    dbCredentials: {
+        //connectionString: process.env.POSTGRES_URL_NON_POOLING as string,
+        connectionString: "postgres://default:z8lxoktUDO3u@ep-icy-haze-43562761-pooler.eu-central-1.postgres.vercel-storage.com/verceldb?ssl=true"
+    },
+    schema: "./db/schema.ts",
+    out: "./db/migrations",
 } satisfies Config;
