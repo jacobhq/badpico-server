af3e20af79ecce1eb84e44d6ff66c7a990120787
Jacob Marshall <me@jhqcat.com>
2023-11-07 22:11:47 +0000

    ✨ Improve API (/next)

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { handle } from 'hono/vercel'
 import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import * as schema from '../db/schema';
-import { desc } from 'drizzle-orm';
+import { desc, gt, lt } from 'drizzle-orm';
 
 export const config = {
   runtime: 'edge',
@@ -32,17 +32,38 @@ const app = new Hono().basePath('/api')
 const db = drizzle(sql, { schema })
 
 app.get('/', async (c) => {
+  // Get latest attack where start time is in past
+  const response = await fetch("http://worldtimeapi.org/api/timezone/Europe/London")
+  const time: Time = await response.json()
+  
   const result = await db.query.attack_schedule.findFirst({
-    orderBy: desc(schema.attack_schedule.id)
+    orderBy: desc(schema.attack_schedule.id),
+    where: lt(schema.attack_schedule.start, new Date(time.datetime))
+  })
+  return c.json(result)
+})
+
+app.get("/next", async (c) => {
+  // Get start time ONLY of latest future attack
+  const response = await fetch("http://worldtimeapi.org/api/timezone/Europe/London")
+  const time: Time = await response.json()
+  
+  const result = await db.query.attack_schedule.findFirst({
+    orderBy: desc(schema.attack_schedule.id),
+    where: gt(schema.attack_schedule.start, new Date(time.datetime)),
+    columns: {
+      start: true
+    }
   })
   return c.json(result)
 })
 
 app.get('/time', async (c) => {
+  // Get current time as ISO8601 datetime
   const response = await fetch("http://worldtimeapi.org/api/timezone/Europe/London")
-  const data: Time = await response.json()
+  const time: Time = await response.json()
 
-  return c.json({ datetime: data.datetime })
+  return c.json({ datetime: time.datetime })
 })
 
 export default handle(app)
