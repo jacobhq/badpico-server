4de808b47830236b8fd6372cafe70d32c1c8589d
Jacob Marshall <me@jhqcat.com>
2023-11-07 20:46:31 +0000

    ✨ Central time sync

--- api/index.ts
+++ api/index.ts
@@ -9,8 +9,26 @@ export const config = {
   runtime: 'edge',
 }
 
-const app = new Hono().basePath('/api')
+type Time = {
+  abbreviation: string
+  client_ip: string,
+  // we only send the client its datetime
+  datetime: string,
+  day_of_week: number,
+  day_of_year: number,
+  dst: boolean,
+  dst_from?: string,
+  dst_offset: number,
+  dst_until?: string,
+  raw_offset: number,
+  timezone: string,
+  unixtime: number,
+  utc_datetime: string,
+  utc_offset: string,
+  week_number: number
+}
 
+const app = new Hono().basePath('/api')
 const db = drizzle(sql, { schema })
 
 app.get('/', async (c) => {
@@ -18,6 +36,13 @@ app.get('/', async (c) => {
     orderBy: desc(schema.attack_schedule.id)
   })
   return c.json(result)
+})
+
+app.get('/time', async (c) => {
+  const response = await fetch("http://worldtimeapi.org/api/timezone/Europe/London")
+  const data: Time = await response.json()
+
+  return c.json({ datetime: data.datetime })
 })
 
 export default handle(app)
